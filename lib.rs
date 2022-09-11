225cbe95f1e8d0f976465b1c5003f9d124ef1b68
Anton <mag-alliance@yandex.ru>
2022-09-11 18:12:13 +0300

    update

--- lib.rs
+++ lib.rs
@@ -4,6 +4,8 @@ use ink_lang as ink;
 
 #[ink::contract]
 pub mod passport {
+    use ink_storage::traits::SpreadAllocate;
+    use ink_prelude::string::String;
     #[ink(storage)]
     #[derive(SpreadAllocate)]
     pub struct Passport {
@@ -20,7 +22,16 @@ pub mod passport {
         /// Creates a new Passport smart contract initialized with the given values
         #[ink(constructor)]
         pub fn new(name: String, surname: String, birth: String, client: String) -> Self {
-            Self { name, surname, birth, assets: Default::default(), client, owner: Self::env().caller(), is_active: true }
+            ink_lang::utils::initialize_contract(|contract: &mut Self| {
+                let caller = Self::env().caller();
+                contract.name = name;
+                contract.surname = surname;
+                contract.birth = birth;
+                contract.client = client;
+                //contract.map.insert(&caller, &0); ?
+                contract.owner = caller;
+                contract.is_active = true;
+            })
         }
 
         /// Returns the current status of the Passport's boolean
@@ -32,24 +43,36 @@ pub mod passport {
         ///  Set passport's status only owner
         #[ink(message)]
         pub fn set_status(&mut self, status: bool) {
-            assert!(self.owner, ink_env::caller());
+            assert_eq!(
+                self.env().caller(),
+                self.owner,
+                "Only owner have permissions to change status"
+            );
             self.is_active = status;
         }
 
         /// Returns the current name of the Passport's
         #[ink(message)]
-        pub fn get_name(&self) -> &str {
-            if self.owner == ink_env::caller() {
-                &format!("{} {}", *self.surname, *self.name)
+        pub fn get_name(&self) -> String {
+            if self.owner == self.env().caller() {
+                let mut full_name = String::from(&self.surname);
+                let _name = &self.name;
+                full_name.push_str(" ");
+                full_name.push_str(_name);
+                return full_name;
             }
-            &self.name
+            String::from(&self.surname)
         }
 
         /// Returns the client info of the Passport's only owner
         #[ink(message)]
-        pub fn get_info(&self) -> &str {
-            assert!(self.owner, ink_env::caller());
-            &self.client
+        pub fn get_info(&self) -> String {
+            assert_eq!(
+                self.env().caller(),
+                self.owner,
+                "Only owner have permissions to get client info"
+            );
+            String::from(&self.client)
         }
     }
 
@@ -61,10 +84,11 @@ pub mod passport {
 
         #[ink::test]
         fn it_works() {
-            let mut passport = Passport::new("name", "surname", "birth", "inn");
+            let mut passport = Passport::new("Ivan".to_string(), "Ivanov".to_string(), "01.01.1970".to_string(), "INN: 123456".to_string());
             assert!(passport.get_status());
             passport.set_status(false);
             assert!(!passport.get_status());
+            assert_eq!(passport.get_name(), "Ivanov Ivan".to_string());
         }
     }
 }
